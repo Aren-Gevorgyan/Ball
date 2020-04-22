60d1156d5068f5710438ba36cb87dcd9aec77a13
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2020-04-22 12:40:53 +0400

    create click in ball and create clone

--- script/script.js
+++ script/script.js
@@ -1,49 +1,83 @@
 $(document).ready(function () {
+    let stopClick = false;
+    let classCount = 1;
 
     class Ball {
         constructor() {
-            this.randomNumber = this.createSizeBall();
+            this.size = 255;
+            this.getWidthWindow = window.innerWidth - this.size;
+            this.getHeightWindow = window.innerHeight - this.size;
+            this.randomNumber = this.getRandomNumber(this.size);
+            this.color = this.createColor();
         }
 
         createBall() {
-            let ball = "<div class = 'ball'></div>";
+            let ball = `<div class = ball${classCount}></div>`;
             $(document.body).append(ball);
-            let styleBall = $(".ball");
+            console.log(classCount);
+            let styleBall = $(`.ball${classCount}`);
             this.createStyleBall(styleBall);
+            classCount++;
         }
 
         createStyleBall(styleBall) {
             styleBall.css({
                 "width": this.randomNumber,
                 "height": this.randomNumber,
-                "left": this.createPositionBallForWidth(),
-                "top": this.createPositionBallForHeight(),
-                "background-color": `rgb(
-                 ${this.createSizeBall()},
-                 ${this.createSizeBall()}, 
-                 ${this.createSizeBall()})`,
+                "left": this.getRandomNumber(this.getWidthWindow),
+                "top": this.getRandomNumber(this.getHeightWindow),
+                "border-radius": "50%",
+                "position": "absolute",
+                "z-index": -10,
+                "background-color": this.color,
             });
-            styleBall.add("class", ".ball");
+        }
+        createColor(){
+            return `rgb(
+                 ${this.getRandomNumber(this.size)},
+                 ${this.getRandomNumber(this.size)}, 
+                 ${this.getRandomNumber(this.size)})`;
         }
 
-        createSizeBall() {
-            return Math.floor(Math.random() * 255);
+        getRandomNumber(num) {
+            return Math.floor(Math.random() * num);
         }
-
-        createPositionBallForWidth() {
-            let getWidthWindow = window.innerWidth - 255;
-            return Math.floor(Math.random() * getWidthWindow);
-        }
-
-        createPositionBallForHeight() {
-            let getHeightWindow = window.innerHeight - 255;
-            return Math.floor(Math.random() * getHeightWindow);
-        }
-
     }
 
-    let ball = new Ball();
-    ball.createBall();
+    start();
+    stop();
 
+    function start() {
+        let startButton = $(".start");
+        startButton.click(createCloneBall);
+    }
+
+    function stop() {
+        let stopButton = $(".stop");
+        stopButton.click(() => {
+            stopClick = true;
+        });
+    }
+
+    function createCloneBall() {
+        new Ball().createBall();
+        stopClick = false;
+        let countObj = setInterval(() => {
+            if (stopClick === true) {
+                clearInterval(countObj);
+            } else {
+                ifClickBallCreateClone();
+            }
+        }, 2500);
+    }
+
+    function ifClickBallCreateClone() {
+        let ball = new Ball();
+        ball.createBall();
+        $("body").children().click(() => {
+            ball.createBall();
+            $(this).animate({left: "50px", top: "50px"});
+        })
+    }
 });
 
